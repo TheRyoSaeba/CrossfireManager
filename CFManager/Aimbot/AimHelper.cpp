6a143037564e5ac96ce32592785f7831584fd128
City Hunter <31343426+TheRyoSaeba@users.noreply.github.com>
2025-09-03 18:24:59 -0500

    Update AimHelper.cpp

--- CFManager/Aimbot/AimHelper.cpp
+++ CFManager/Aimbot/AimHelper.cpp
@@ -61,7 +61,8 @@ void outlined_text(const ImVec2& pos, ImU32 color, const char* text) {
 }
 
  
- 
+// if these two aimbots are odd snd funky that's  because idk how to make an aimbot so i gsve up and asked a chatbot for help lmao
+//sorry 
 
 void MoveCursor(float targetX, float targetY, LTClientShell _shell)
 {
